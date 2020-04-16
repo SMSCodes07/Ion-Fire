5b2b1064ae75349340d6ca151c551a13eb39bcff
SMSCodes07 <48341791+SMSCodes07@users.noreply.github.com>
2020-04-15 20:07:08 -0400

    Home Commit
    En este commit se creo la pantalla inicial de la App, la cual puede redireccionar a los usuarios al formulario de login o al de registro, segun les convenga. Se agrego un nuevo archivo de multimedia (Ilustracion).

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private router: Router) {}
+  // Funciones para navegar entre las pantallas
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
+  goToRegister() {
+    this.router.navigate(['/register']);
+  }
+  // Funciones para navegar entre las pantallas
 
 }
